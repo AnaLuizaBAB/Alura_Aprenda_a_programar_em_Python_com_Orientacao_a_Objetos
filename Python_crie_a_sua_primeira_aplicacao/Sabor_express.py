0191f280a23b373265f85417fe9f32caf2f1c412
AnaLuizaBAB <aninhat17@gmail.com>
2024-04-20 19:32:14 -0300

    Módulo 2

--- Python_crie_a_sua_primeira_aplicacao/Sabor_express.py
+++ Python_crie_a_sua_primeira_aplicacao/Sabor_express.py
@@ -1,32 +1,45 @@
 import os
 
-print(""" 
-                                                    ╔═══╗──╔╗───────╔═══╗
-                                                    ║╔═╗║──║║───────║╔══╝
-                                                    ║╚══╦══╣╚═╦══╦═╗║╚══╦╗╔╦══╦═╦══╦══╦══╗
-                                                    ╚══╗║╔╗║╔╗║╔╗║╔╝║╔══╩╬╬╣╔╗║╔╣║═╣══╣══╣
-                                                    ║╚═╝║╔╗║╚╝║╚╝║║─║╚══╦╬╬╣╚╝║║║║═╬══╠══║
-                                                    ╚═══╩╝╚╩══╩══╩╝─╚═══╩╝╚╣╔═╩╝╚══╩══╩══╝
-                                                    ───────────────────────║║
-                                                    ───────────────────────╚╝ """)
+def exibir_nome_do_programa():
+    print(""" 
+                                                        ╔═══╗──╔╗───────╔═══╗
+                                                        ║╔═╗║──║║───────║╔══╝
+                                                        ║╚══╦══╣╚═╦══╦═╗║╚══╦╗╔╦══╦═╦══╦══╦══╗
+                                                        ╚══╗║╔╗║╔╗║╔╗║╔╝║╔══╩╬╬╣╔╗║╔╣║═╣══╣══╣
+                                                        ║╚═╝║╔╗║╚╝║╚╝║║─║╚══╦╬╬╣╚╝║║║║═╬══╠══║
+                                                        ╚═══╩╝╚╩══╩══╩╝─╚═══╩╝╚╣╔═╩╝╚══╩══╩══╝
+                                                        ───────────────────────║║
+                                                        ───────────────────────╚╝ """)
 
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurantes")
+    print("3. Ativar Restaurante")
+    print("4. Sair")
 
 def finalizando_app():
     os.system("cls")                    # Limpa a tela do terminal antes de imprimir a mensagem "Encerrando" o programa"
     print("Encerrando Programa\n")
 
-print("1. Cadastrar Restaurante")
-print("2. Listar Restaurantes")
-print("3. Ativar Restaurante")
-print("4. Sair")
+def escolher_opcao():
+    opcao_escolhida = int(input("\nEscolha uma opçao: "))
 
-opcao_escolhida = int(input("\nEscolha uma opçao: "))
+    match opcao_escolhida:
+        case 1:
+            print("Cadastrar Restaurante")
+        case 2:
+            print("Listar Restaurantes")
+        case 3:
+            print("Ativar Restaurante")
+        case 4:
+            finalizando_app()
 
-if opcao_escolhida == 1:
-    print("Cadastrar Restaurante")
-if opcao_escolhida == 2:
-    print("Listar Restaurantes")
-if opcao_escolhida == 3:
-    print("Ativar Restaurante")
-if opcao_escolhida == 4:
-    finalizando_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
